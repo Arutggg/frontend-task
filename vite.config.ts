0498e5d7af64741ae629762c2bb333af646bd130
Arutggg <arutyun.gaz@bk.ru>
2026-05-16 15:07:59 +0300

    chore: setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // GitHub Pages хостит сайт по пути /frontend-task/ 
+  // чтобы правильно сформировать пути к JS/CSS/картинкам в билде.
+  base: '/frontend-task/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
